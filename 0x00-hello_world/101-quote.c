8222ae22a01651de314cb5a8e2425605e036d3a4
Deekay69 <kunguduncan19481@gmail.com>
2022-09-09 00:57:34 -0700

    tyty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - entrypoint
- * return : always 1 (success)
+ * main - main block
+ * return :1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+			"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+			 s);
 	return (1);
 }

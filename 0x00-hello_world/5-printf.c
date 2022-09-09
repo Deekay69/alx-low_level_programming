0df6dafcd2e4c62e2da5a0c1d42d97152deb81d8
Deekay69 <kunguduncan19481@gmail.com>
2022-09-09 00:19:35 -0700

    rere

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** Print the words
+ *
+ * return  (success)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}

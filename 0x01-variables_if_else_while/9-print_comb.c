109e9b3e41d3f8899b17eda46627c55f1cf31cbe
Deekay69 <kunguduncan19481@gmail.com>
2022-09-11 00:04:18 -0700

    ty

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main body
+ * Description: prints all possible combinations of single-digit numbers in ascending order
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}

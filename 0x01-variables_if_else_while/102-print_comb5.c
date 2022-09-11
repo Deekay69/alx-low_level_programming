6cd8f2fe4202fc5988f412658aaad0c32b1a1656
Deekay69 <kunguduncan19481@gmail.com>
2022-09-11 02:10:04 -0700

    ty

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main body
+ * Decription: Prints combinations to 99
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '98'; x++)
+	{
+		for (y = x + 1; y <= '99'; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+
+			if (x == '98' && y == '99')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}

39a253031dafbf7ce9394623fdd0cae446557d4a
Deekay69 <kunguduncan19481@gmail.com>
2022-09-14 00:20:37 -0700

    ty

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @a: Value to be used
+ * @b: interger to be used
+ * Return: Always success
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}

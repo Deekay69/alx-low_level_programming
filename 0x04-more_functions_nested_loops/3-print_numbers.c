e02fb4d40c876665d59eb7b6f810d1c1dd0215d8
Deekay69 <kunguduncan19481@gmail.com>
2022-09-15 11:36:23 -0700

    u

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- * Description: Putchar used twice
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
  */
 
 void print_numbers(void)

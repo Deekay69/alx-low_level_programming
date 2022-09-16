081328694fc482b595efde6a79336bf9b4bc135d
Deekay69 <kunguduncan19481@gmail.com>
2022-09-16 00:34:13 -0700

    ty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (i < 10)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar('0' + i);
 		}
 		_putchar('\n');
 	}

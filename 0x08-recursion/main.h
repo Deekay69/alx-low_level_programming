1e35b6134f9bf13fcb902525b5c66ff84324f368
Deekay69 <kunguduncan19481@gmail.com>
2022-09-28 23:45:03 -0700

    ty

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,9 @@ int is_prime_number(int n);
 int halp(int c, int i);
 int hai(int n, int d);
 int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
+int my_pal(char *s, int l);
+int get_length(char *s);
 

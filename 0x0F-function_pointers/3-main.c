d2b6f583a060b1ecb95789a3de93566553560d23
Deekay69 <kunguduncan19481@gmail.com>
2022-10-15 08:37:07 -0700

    ty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,12 +23,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	o = get_op_func(argv[2]);
 	if (o == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 

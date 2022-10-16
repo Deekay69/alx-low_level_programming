25c6ec77ccc2a2c16c4585338dbdcb4b2d8b9218
Deekay69 <kunguduncan19481@gmail.com>
2022-10-15 17:52:33 -0700

    ty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,38 +1,61 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - main function
- * @argc: argument count
- * @argv: string of arguments in array
- * Return: 0
+ * op_add - calculate the sum of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: sum of a and b
  */
-int main(int argc, char *argv[])
+int op_add(int a, int b)
 {
-	int a, b;
-	int (*o)(int, int);
+	return (a + b);
+}
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	o = get_op_func(argv[2]);
-	if (o == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+/**
+ * op_sub -calculates the difference of two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-	printf("%d\n", o(a, b));
+/**
+ * op_mul - calculates the multiplication of two numbers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return ( a * b);
+}
 
-	return (0);
+/**
+ * op_div - divides two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: result of dividion od a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: result of the the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
